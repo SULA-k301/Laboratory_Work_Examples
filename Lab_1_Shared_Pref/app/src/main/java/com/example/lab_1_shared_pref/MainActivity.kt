083cce2410b71b5affe431bb510c5ac9ddb3e3f1
k301piavka <y.piavka@khai.edu>
2021-09-14 08:29:00 +0300

    initial commit

---
+++ Lab_1_Shared_Pref/app/src/main/java/com/example/lab_1_shared_pref/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lab_1_shared_pref
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
